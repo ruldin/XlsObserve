5e85f27c853dedbae85ef183d34eee7082c61727
Ruldin Ayala <ruldin.ramos@fan.gov>
2022-05-21 23:25:15 -0600

    Add Omited file

---
+++ XlsObserve/Class/logs/ClsLogs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlsObserve.Class.logs
+{
+    public static class ClsLogs
+    {
+        public static readonly int info = 1;
+        public static readonly int actions = 2;
+
+        public static void StatusTrace(string msg,int level)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+        public  static void ErrorLog(string message)
+        {
+            Console.WriteLine($"error {message}");
+        }
+
+        
+
+
+
+
+    }
+}
